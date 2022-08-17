600820e5eaff730780dc8ee58efa11e3bfa064a5
tomoko.nakatani <Tomoko_Nakatani@jrass.jp>
2022-08-17 22:08:55 +0900

    Initial Commit

---
+++ HelloWorld/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by PC-SYSKAI555 on 2022/08/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
